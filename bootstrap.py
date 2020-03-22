a6a6324a99f58a257c25b05fe0fe298454eec64e
Alex Coplan <lexy0202@gmail.com>
2020-03-22 21:33:17 +0000

    make script quieter

--- bootstrap.py
+++ bootstrap.py
@@ -4,11 +4,42 @@ import subprocess
 import os
 import stat
 import platform
+import sys
 
-def run(cmd):
-  cmd_s = " ".join(cmd)
-  print(f"Running: {cmd_s}")
-  subprocess.check_call(cmd)
+logbuf = ""
+quiet = True
+
+def log(msg):
+  global logbuf
+  logbuf += msg
+  if not quiet:
+    sys.stdout.write(msg)
+
+def logln(msg):
+  log(msg + "\n")
+
+def fail():
+  if quiet: # if we have been keeping quiet, print log up to this point.
+    print(logbuf)
+  exit(1)
+
+def run(cmd, quiet, **kwargs):
+  log("Running " + " ".join(cmd) + " ... ")
+  res = subprocess.run(cmd, capture_output=True, **kwargs)
+  if res.returncode == 0:
+    logln("OK")
+    return
+  logln("FAILED")
+  stdout = res.stdout.decode("utf-8").strip()
+  stderr = res.stderr.decode("utf-8").strip()
+  if len(stdout) > 0:
+    logln("Output (stdout):")
+    log(stdout)
+  if len(stderr) > 0:
+    logln("Output (stderr):")
+    log(stderr)
+
+  fail()
 
 def plat_str():
   return platform.system().lower()
@@ -30,7 +61,7 @@ def main():
     os.makedirs(build_dir)
 
   heap_path = artefact(heap_name)
-  run(['ml-build', 'heap2asm.cm', 'Main.main', heap_path])
+  run(['ml-build', 'heap2asm.cm', 'Main.main', heap_path], quiet)
 
   script_path = artefact(script_name)
   with open(script_path, 'w') as f:
@@ -40,8 +71,8 @@ def main():
   make_executable(script_path)
 
   out_path = artefact('heap2asm')
-  run(['./heap2exec', script_path, heap_path, out_path])
-  print(f"All done. Final executable in {out_path}")
+  run(['./heap2exec', script_path, heap_path, out_path], quiet)
 
 if __name__ == '__main__':
+  quiet = False # if running on command-line, be verbose
   main()
